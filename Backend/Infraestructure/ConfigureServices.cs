2f38aff22d6905349841823f01cfce413c16822c
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2024-09-02 22:28:48 -0300

    Infraestructure layer configurations

---
+++ Backend/Infraestructure/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfraestructureServices(this IServiceCollection services) {
+
+
+            return services;
+        }
+    }
+}
