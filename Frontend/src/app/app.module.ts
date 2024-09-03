3ee191ca0d6943c2825ce314b12b38ee35d9eec5
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2024-09-03 00:12:08 -0300

    NGRX and primeng configurations

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({},{}),
+    StoreDevtoolsModule.instrument({name: 'TEST'})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+import { Environment } from "./environment.interface";
+
+export const environment : Environment = {
+  production: false,
+  name: "local",
+  apiUrl: "https://localhost:7025",
+  version: "0.0.0.0"
+};
