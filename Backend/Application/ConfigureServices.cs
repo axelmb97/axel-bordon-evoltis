9d32fdeb375b2a2a806c50887bfd2cf9daa63d1f
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2024-09-02 22:23:28 -0300

    Application layer configurations

---
+++ Backend/Application/ConfigureServices.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Mapings;
+using AutoMapper;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationSerrvices(this IServiceCollection services) {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
